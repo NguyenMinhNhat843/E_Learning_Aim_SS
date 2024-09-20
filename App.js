dcb996e1a9962cf21a36175a5458c7328ff1b0ed
LAPTOP-92NBAG1S\lenovo <nomai6789@gmail.com>
2024-09-20 21:39:36 +0700

    create and code screen UserProfile

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//PhungCanhTuan
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import UserProfile from "./screens/UserProfile"; 
+// import Detail from "./screens/";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="UserProfile"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="UserProfile" component={UserProfile} />
+        {/* <Stack.Screen name="" component={} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
