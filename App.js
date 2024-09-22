b600e243e8fa393cda69b544d3b6c55884be115a
LAPTOP-92NBAG1S\lenovo <nomai6789@gmail.com>
2024-09-22 16:36:56 +0700

    create and code screen techerProfile

--- App.js
+++ App.js
@@ -6,19 +6,21 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import UserProfile from "./screens/UserProfile"; 
 import MyCourses from "./screens/MyCourses";
+import TeacherProfile from "./screens/teacherProfile";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="MyCourses"
+        initialRouteName="TeacherProfile"
         screenOptions={{
           headerShown: false,
         }}
       >
         <Stack.Screen name="UserProfile" component={UserProfile} />
         <Stack.Screen name="MyCourses" component={MyCourses} />
+        <Stack.Screen name="TeacherProfile" component={TeacherProfile} />
       </Stack.Navigator>
     </NavigationContainer>
   );
