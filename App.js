d34b88f4c2e65483a332cab75c5633197e7b4b90
NguyenMinhNhat843 <NhatNguyen15062019@gmail.com>
2024-09-24 15:40:40 +0700

    coursedetail lesson

--- App.js
+++ App.js
@@ -1,34 +1,36 @@
 //PhungCanhTuan
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import UserProfile from "./screens/UserProfile"; 
-import MyCourses from "./screens/MyCourses";
+import UserProfile from './screens/UserProfile';
+import MyCourses from './screens/MyCourses';
+import CourseDetail from './screens/CourseDetail';
 
 export default function App() {
-  const Stack = createNativeStackNavigator();
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="MyCourses"
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="UserProfile" component={UserProfile} />
-        <Stack.Screen name="MyCourses" component={MyCourses} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    const Stack = createNativeStackNavigator();
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName="CourseDetail"
+                screenOptions={{
+                    headerShown: true,
+                }}
+            >
+                <Stack.Screen name="UserProfile" component={UserProfile} />
+                <Stack.Screen name="MyCourses" component={MyCourses} />
+                <Stack.Screen name="CourseDetail" component={CourseDetail} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
